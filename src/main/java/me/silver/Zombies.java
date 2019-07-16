54c6c2e30aef37e505e8bff778560fe40ba62660
KevlarBullet <ian.kellogg001@gmail.com>
2019-07-16 12:47:45 -0800

    Added static instance thing
    Because I'm too lazy to add dependency injection

--- src/main/java/me/silver/Zombies.java
+++ src/main/java/me/silver/Zombies.java
@@ -3,13 +3,15 @@ package me.silver;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Zombies extends JavaPlugin {
-//    @Override
-//    public void onDisable() {
-//
-//    }
+
+    private static Zombies plugin;
 
     @Override
     public void onEnable() {
+        plugin = this;
+    }
 
+    public static Zombies getInstance() {
+        return plugin;
     }
 }
