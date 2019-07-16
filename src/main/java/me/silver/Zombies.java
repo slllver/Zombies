772d06bde7f62cae4731a1b6375c6fabcaecf0d0
KevlarBullet <ian.kellogg001@gmail.com>
2019-07-16 01:05:29 -0800

    Initial commit

---
+++ src/main/java/me/silver/Zombies.java
@@ -0,0 +1,15 @@
+package me.silver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Zombies extends JavaPlugin {
+//    @Override
+//    public void onDisable() {
+//
+//    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
