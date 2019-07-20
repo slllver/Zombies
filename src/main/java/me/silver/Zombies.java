418f729f3bcb8ffe8bcb9f7f71609e0fec49ea8a
KevlarBullet <ian.kellogg001@gmail.com>
2019-07-19 18:52:36 -0800

    Added PlayerListener class and implemented custom AI Tasks

--- src/main/java/me/silver/Zombies.java
+++ src/main/java/me/silver/Zombies.java
@@ -1,5 +1,13 @@
 package me.silver;
 
+import me.silver.listener.PlayerListener;
+import me.silver.mob.MineZombie;
+import me.silver.util.NMSUtils;
+import net.minecraft.server.v1_12_R1.ChatMessageType;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Zombies extends JavaPlugin {
@@ -9,6 +17,20 @@ public class Zombies extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        registerEntities();
+    }
+
+    private void registerEntities() {
+        NMSUtils.registerEntity("mine_zombie", NMSUtils.Type.ZOMBIE, MineZombie.class, false);
+    }
+
+    public static void sendActionBar(Player player, String message) {
+        IChatBaseComponent cbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + message + "\"}");
+        PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, ChatMessageType.GAME_INFO);
+        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(ppoc);
     }
 
     public static Zombies getInstance() {
---
+++ src/main/java/me/silver/listener/PlayerListener.java
@@ -0,0 +1,67 @@
+package me.silver.listener;
+
+
+import me.silver.Zombies;
+import me.silver.util.PlayerSpeed;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+
+import java.text.DecimalFormat;
+
+public class PlayerListener implements Listener {
+
+    private static Zombies plugin = Zombies.getInstance();
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+            PlayerSpeed playerSpeed = PlayerSpeed.getPlayerSpeed(player.getName());
+            playerSpeed.setSpeed(playerSpeed.getSpeed().setY(0.0));
+
+            Zombies.sendActionBar(player, new DecimalFormat("#.###").format(playerSpeed.getSpeed().length()));
+        }, 0, 1);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        Vector from = event.getFrom().toVector();
+        Vector to = event.getTo().toVector();
+        Vector speed = to.subtract(from);
+
+        // Set the player's speed to 0 after 5 ticks if the player hasn't moved
+        int removeSpeedTask = plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            if (PlayerSpeed.containsPlayer(player.getName())) {
+                PlayerSpeed playerSpeed = PlayerSpeed.getPlayerSpeed(player.getName());
+                playerSpeed.setSpeed(new Vector(0, 0, 0));
+            }
+        }, 5L);
+
+        if (PlayerSpeed.containsPlayer(player.getName())) {
+            PlayerSpeed playerSpeed = PlayerSpeed.getPlayerSpeed(player.getName());
+
+            if (plugin.getServer().getScheduler().isQueued(playerSpeed.getTaskId())) {
+                plugin.getServer().getScheduler().cancelTask(playerSpeed.getTaskId());
+            }
+
+            playerSpeed.setSpeed(speed);
+            playerSpeed.setTaskId(removeSpeedTask);
+
+            Vector notY = new Vector(speed.getX(), 0, speed.getZ());
+            if (notY.length() > 0) {
+                plugin.getLogger().info(new DecimalFormat("#.###").format(notY.length()));
+            }
+        } else {
+            PlayerSpeed.add(new PlayerSpeed(player.getName(), speed, removeSpeedTask));
+        }
+
+    }
+}
